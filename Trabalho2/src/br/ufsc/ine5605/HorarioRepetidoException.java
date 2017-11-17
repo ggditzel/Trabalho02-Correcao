ae1fdc6d69133e3a2b3b8ade9b87fce5e8b06b6c
ggditzel <ggditzel@gmail.com>
2017-11-17 08:28:10 -0200

    Tentativa de reduzir pastas desnecessárias do projeto

--- Trabalho2/src/br/ufsc/ine5605/HorarioRepetidoException.java
+++
@@ -1,7 +0,0 @@
-package br.ufsc.ine5605;
-
-public class HorarioRepetidoException extends Exception{
-	public HorarioRepetidoException() {
-		super("O cargo ja possui este horario de acesso.");
-	}
-}
