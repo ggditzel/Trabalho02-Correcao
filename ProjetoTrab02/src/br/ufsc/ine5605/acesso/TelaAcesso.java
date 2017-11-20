76a1d20618b7bd279843ace9b313d84138d32b19
mateuscechetto <32515099+mateuscechetto@users.noreply.github.com>
2017-11-20 00:09:43 -0200

    Update TelaAcesso.java

--- ProjetoTrab02/src/br/ufsc/ine5605/acesso/TelaAcesso.java
+++ ProjetoTrab02/src/br/ufsc/ine5605/acesso/TelaAcesso.java
@@ -15,14 +15,14 @@ import br.ufsc.ine5605.TelaComGridBagLayout;
 import br.ufsc.ine5605.horario.ControladorHorario;
 import br.ufsc.ine5605.horario.Hora;
 
-public class TelaAcesso extends TelaComGridBagLayout{
-	private JLabel lbPergunta;
-	private JTextField tfMatricula;
-	private JButton btConfirma;
+public class TelaAcesso extends TelaComGridBagLayout implements ActionListener{
+	protected JLabel lbPergunta;
+	protected JTextField tfMatricula;
+	protected JButton btConfirma;
 	
-	private int matricula;
-	private String data;
-	private Hora hora;
+	protected int matricula;
+	protected String data;
+	protected Hora hora;
 	
 	public TelaAcesso() {
 		this(3);
@@ -31,29 +31,27 @@ public class TelaAcesso extends TelaComGridBagLayout{
 		this.data = formata.format(agora);
 		formata = new SimpleDateFormat("HH:mm");
 		hora = ControladorHorario.getInstance().converte(formata.format(agora));
-	}
+        }
 	
-	protected TelaAcesso(int nComponentes) {
+	protected TelaAcesso(int nLinhasComponentes) {
 		super("Acesso ao Financeiro");
 		
 		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setSize(400, 250);
-		setAlwaysOnTop(true);
 		setLocationRelativeTo(null);
 		
-		ButtonActionListener btListener = new ButtonActionListener();
 		
 		lbPergunta = new JLabel("Insira sua matricula :");
 		constraints.fill = GridBagConstraints.HORIZONTAL;
-		adicionaComponente(lbPergunta, 0, nComponentes - 3, 1, 1);
+		adicionaComponente(lbPergunta, 0, nLinhasComponentes - 3, 1, 1);
 		
 		tfMatricula = new JTextField();
-		adicionaComponente(tfMatricula, 0, nComponentes -2, 3, 1);
+		adicionaComponente(tfMatricula, 0, nLinhasComponentes -2, 3, 1);
 		
 		btConfirma = new JButton("Confirmar");
-		btConfirma.addActionListener(btListener);
-		adicionaComponente(btConfirma, 0, nComponentes - 1, 1, 1);
+		btConfirma.addActionListener(this);
+		adicionaComponente(btConfirma, 0, nLinhasComponentes - 1, 1, 1);
 		
 	}
 	
@@ -63,30 +61,34 @@ public class TelaAcesso extends TelaComGridBagLayout{
 	public void setHora(Hora hora) {
 		this.hora = hora;
 	}
-	
+	/**
+         * Abre janela mostrando mensagem de erro
+         * @param motivo 
+         */
 	public void mostraNegacao(MotivoNegacaoAcesso motivo) {
 		JOptionPane.showMessageDialog(null, motivo.getNome(), "Acesso Negado", JOptionPane.ERROR_MESSAGE);
 	}
+        /**
+         * Abre janela confirmando acesso
+         */
 	public void confirmaAcesso() {
 		JOptionPane.showMessageDialog(null,"Acesso Permitido");
 	}
-	
-	private class ButtonActionListener implements ActionListener {
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			boolean respostaOk = true;
-			try {
-				matricula = Integer.parseInt(tfMatricula.getText());
-			} catch (NumberFormatException exception) {
-				JOptionPane.showMessageDialog(null, "Digite apenas numeros para a matricula", "Formato de Matricula invalido", JOptionPane.ERROR_MESSAGE);
-				respostaOk = false;
-			}
-			if(respostaOk) {
-				ControladorTentativaAcesso.getInstance().validaTentativa(matricula, data, hora);
-				tfMatricula.setText("");
-			}
-		}
-	
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        boolean respostaOk = true;
+	try {
+            matricula = Integer.parseInt(tfMatricula.getText());
+	} catch (NumberFormatException exception) {
+            JOptionPane.showMessageDialog(null, "Digite apenas numeros para a matricula", "Formato de Matricula invalido", JOptionPane.ERROR_MESSAGE);
+            respostaOk = false;
 	}
+	if(respostaOk) {
+            ControladorTentativaAcesso.getInstance().validaTentativa(matricula, data, hora);
+            tfMatricula.setText("");
+	}
+    }
+	
+
 }
